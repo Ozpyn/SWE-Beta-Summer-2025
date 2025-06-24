ef254a50aa877481d5546368d34d10918d3ef899
Ozpyn <gmingle@kent.edu>
2025-06-24 11:18:33 -0400

    fixed text rotating in cards

--- p5-cards/components/card.js
+++ p5-cards/components/card.js
@@ -16,10 +16,7 @@ class Card {
     // Image loading
     this.suitImage;
     this.rankImage;
-    this.imagesLoaded = false;
     this.setImages();
-
-
   }
 
   async setImages() {
@@ -36,7 +33,7 @@ class Card {
         default:
           break;
       }
-      if (!['joker', 'ace'].includes(this.rank.toLowerCase())) {
+      if (!(this.rank === 'Joker' || this.rank === 'Ace')) {
         switch (this.suit.toLowerCase()) {
           case 'club': this.suitImage = club;
             break;
@@ -49,7 +46,7 @@ class Card {
           default:
             break;
         }
-      } else if (['ace'].includes(this.rank.toLowerCase())){
+      } else if (this.rank === 'Ace'){
         switch (this.suit.toLowerCase()) {
           case 'club': this.suitImage = d_club;
             break;
@@ -67,8 +64,6 @@ class Card {
       }
   }
   
-
-  // Original draw method (you can modify this one to pick what to show)
   draw(x = this.x, y = this.y) {
     if (this.faceUp) {
       this.drawFront()
@@ -87,6 +82,7 @@ class Card {
   }
 
   drawFront(x = this.x, y = this.y) {
+    push();
     // Draw card background
     fill(255);
     stroke(0);
@@ -102,7 +98,7 @@ class Card {
     
     // Draw rank and suit in top left
     fill(suitColor);
-    textSize(10);
+    textSize(12);
     textAlign(LEFT, TOP);
     
     if (this.rankImage) {
@@ -114,10 +110,14 @@ class Card {
       text(this.rank, x + 2, y + 2);
     }
     
-    // Draw large suit symbol in center
+    // Draw large suit symbol in center, larger for aces who have details
     if (this.suitImage) {
       imageMode(CENTER);
-      image(this.suitImage, x + this.width/2, y + this.height/2, 25, 25);
+      if (this.rank === 'Ace') {
+        image(this.suitImage, x + this.width / 2, y + this.height / 2, 40, 40);
+      } else {
+        image(this.suitImage, x + this.width / 2, y + this.height / 2, 25, 25);
+      }
     } else {
       // Fallback large suit symbol
       textSize(20);
@@ -126,23 +126,18 @@ class Card {
     }
     
     // Draw rank and suit in bottom right (rotated)
-    textSize(10);
-    textAlign(RIGHT, BOTTOM);
-    push();
+    textAlign(LEFT, TOP); // Because after rotation, (0,0) is effectively top-left of the rotated object
+
     translate(x + this.width - 2, y + this.height - 2);
     rotate(PI);
-    
+
     if (this.rankImage) {
-      imageMode(CORNER);
+      imageMode(CORNER); // CORNER works fine here with proper alignment
       image(this.rankImage, 0, 0, 20, 20);
     } else {
       text(this.rank, 0, 0);
-      // text(this.getSuitSymbol(), 15, 0);
     }
     pop();
-    
-    // Reset text alignment
-    textAlign(LEFT, BASELINE);
   }
 
   getSuitSymbol() {
